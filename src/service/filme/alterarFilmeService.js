da5bfef61b788fb754a55a37461be4e7feb6badf
Leonardo <leonardosantiagodesouza@outlook.com.br>
2025-03-30 17:15:35 -0300

    30/03/2025.2

--- src/service/filme/alterarFilmeService.js
+++ src/service/filme/alterarFilmeService.js
@@ -1,7 +1,9 @@
 import { alterarFilme } from "../../repository/filmeRepository.js";
+import { validarNovoFilme } from "../../validation/filme/filmeValidation.js";
 
 export default async function  alterarFilmeService(filmeObj,id) {
-    
+    //valida campos obrigatários
+    validarNovoFilme(filmeObj)
     let linhasAfetadas= await alterarFilme(filmeObj,id);
     if(linhasAfetadas == 0)
         throw new Error('Nenhuma linha afetada.');
